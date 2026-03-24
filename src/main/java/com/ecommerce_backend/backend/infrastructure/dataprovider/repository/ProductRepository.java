046771642457202e67ba7da4b9acecb7d87e69b7
Tarcisio <bluemarblenetwork@gmail.com>
2026-03-24 10:58:57 -0300

    fix:repository

--- src/main/java/com/ecommerce_backend/backend/infrastructure/dataprovider/repository/ProductRepository.java
+++ src/main/java/com/ecommerce_backend/backend/infrastructure/dataprovider/repository/ProductRepository.java
@@ -3,9 +3,11 @@ package com.ecommerce_backend.backend.infrastructure.dataprovider.repository;
 import com.ecommerce_backend.backend.infrastructure.persistence.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional; // Não esqueça o import
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-    // Pode adicionar métodos customizados como findBySku se necessário
 
+    // Adicione esta linha para o Spring gerar a busca por SKU
+    Optional<ProductEntity> findBySku(String sku);
 }
